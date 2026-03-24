732614088cad7b2895f47d610787dfd2cc36f53e
Mauro <maurotto80@gmail.com>
2026-03-24 19:04:39 +0100

    Insert Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+//app/layout.tsx
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -57,11 +60,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="it">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-8LDCY09NVY"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-8LDCY09NVY');
+    `}
+  </Script>
+
+  <body
+    className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+  >
+    {children}
+  </body>
+
+</html>
   );
 }
